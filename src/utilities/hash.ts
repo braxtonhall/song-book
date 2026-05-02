a35d52070526f9eab4fc28bc491de920f9017544
braxtonhall <braxtonjhall@gmail.com>
2026-05-02 13:13:34 +0200

    Add more sample data

--- src/utilities/hash.ts
+++ src/utilities/hash.ts
@@ -17,3 +17,15 @@ export function stringToColour(str: string): string {
 
 	return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
 }
+
+export function hashString(str: string): number {
+	let hash = 0;
+
+	for (let i = 0; i < str.length; i++) {
+		const char = str.charCodeAt(i);
+		hash = (hash << 5) - hash + char;
+		hash |= 0; // Convert to 32-bit integer
+	}
+
+	return hash;
+}
